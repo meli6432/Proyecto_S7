71f572842cfa98e1c87bc704a67a10972e461412
meliv <meli6432@gmail.com>
2025-01-24 23:19:19 -0500

    añadimos aplicación web

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header("Aplicación sobre vehículos usados")
+
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig_1 = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_1, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un gráfico de dispersión')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un gráfico de dispersión para las columnas odómetro y precio')
+    
+    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
+    st.plotly_chart(fig_2, use_container_width=True)
